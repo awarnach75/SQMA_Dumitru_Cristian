0dee33432b740b6560adb4730932e26e9054f666
DUMITRU N CRISTIAN IONUT <cristi.dumitru99@yahoo.com>
2026-01-15 09:19:59 +0200

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class MyHomeworkTests(unittest.TestCase):
+
+    def test_one(self):
+        print("Running Test One...")
+        self.assertTrue(True)
+
+    def test_two(self):
+        print("Running Test Two...")
+        self.assertEqual(1 + 1, 3)
+
+if __name__ == '__main__':
+    unittest.main()
